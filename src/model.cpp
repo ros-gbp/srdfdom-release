3e467c1a5086f9fb9154cc9c6f643b011242626b
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-08-19 22:25:43 +0200

    Imported upstream version '0.6.0' of 'upstream'

--- src/model.cpp
+++ src/model.cpp
@@ -1,36 +1,36 @@
 /*********************************************************************
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2011, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 
 /* Author Ioan Sucan */
 
@@ -43,9 +43,11 @@
 #include <set>
 #include <limits>
 
-void srdf::Model::loadVirtualJoints(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+using namespace tinyxml2;
+
+void srdf::Model::loadVirtualJoints(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* vj_xml = robot_xml->FirstChildElement("virtual_joint"); vj_xml;
+  for (XMLElement* vj_xml = robot_xml->FirstChildElement("virtual_joint"); vj_xml;
        vj_xml = vj_xml->NextSiblingElement("virtual_joint"))
   {
     const char* jname = vj_xml->Attribute("name");
@@ -98,9 +100,9 @@ void srdf::Model::loadVirtualJoints(const urdf::ModelInterface& urdf_model, TiXm
   }
 }
 
-void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* group_xml = robot_xml->FirstChildElement("group"); group_xml;
+  for (XMLElement* group_xml = robot_xml->FirstChildElement("group"); group_xml;
        group_xml = group_xml->NextSiblingElement("group"))
   {
     const char* gname = group_xml->Attribute("name");
@@ -114,7 +116,7 @@ void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, TiXmlElemen
     boost::trim(g.name_);
 
     // get the links in the groups
-    for (TiXmlElement* link_xml = group_xml->FirstChildElement("link"); link_xml;
+    for (XMLElement* link_xml = group_xml->FirstChildElement("link"); link_xml;
          link_xml = link_xml->NextSiblingElement("link"))
     {
       const char* lname = link_xml->Attribute("name");
@@ -133,7 +135,7 @@ void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, TiXmlElemen
     }
 
     // get the joints in the groups
-    for (TiXmlElement* joint_xml = group_xml->FirstChildElement("joint"); joint_xml;
+    for (XMLElement* joint_xml = group_xml->FirstChildElement("joint"); joint_xml;
          joint_xml = joint_xml->NextSiblingElement("joint"))
     {
       const char* jname = joint_xml->Attribute("name");
@@ -162,7 +164,7 @@ void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, TiXmlElemen
     }
 
     // get the chains in the groups
-    for (TiXmlElement* chain_xml = group_xml->FirstChildElement("chain"); chain_xml;
+    for (XMLElement* chain_xml = group_xml->FirstChildElement("chain"); chain_xml;
          chain_xml = chain_xml->NextSiblingElement("chain"))
     {
       const char* base = chain_xml->Attribute("base_link");
@@ -221,7 +223,7 @@ void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, TiXmlElemen
     }
 
     // get the subgroups in the groups
-    for (TiXmlElement* subg_xml = group_xml->FirstChildElement("group"); subg_xml;
+    for (XMLElement* subg_xml = group_xml->FirstChildElement("group"); subg_xml;
          subg_xml = subg_xml->NextSiblingElement("group"))
     {
       const char* sub = subg_xml->Attribute("name");
@@ -280,9 +282,9 @@ void srdf::Model::loadGroups(const urdf::ModelInterface& urdf_model, TiXmlElemen
   }
 }
 
-void srdf::Model::loadGroupStates(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+void srdf::Model::loadGroupStates(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* gstate_xml = robot_xml->FirstChildElement("group_state"); gstate_xml;
+  for (XMLElement* gstate_xml = robot_xml->FirstChildElement("group_state"); gstate_xml;
        gstate_xml = gstate_xml->NextSiblingElement("group_state"))
   {
     const char* sname = gstate_xml->Attribute("name");
@@ -316,7 +318,7 @@ void srdf::Model::loadGroupStates(const urdf::ModelInterface& urdf_model, TiXmlE
     }
 
     // get the joint values in the group state
-    for (TiXmlElement* joint_xml = gstate_xml->FirstChildElement("joint"); joint_xml;
+    for (XMLElement* joint_xml = gstate_xml->FirstChildElement("joint"); joint_xml;
          joint_xml = joint_xml->NextSiblingElement("joint"))
     {
       const char* jname = joint_xml->Attribute("name");
@@ -376,9 +378,9 @@ void srdf::Model::loadGroupStates(const urdf::ModelInterface& urdf_model, TiXmlE
   }
 }
 
-void srdf::Model::loadEndEffectors(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+void srdf::Model::loadEndEffectors(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* eef_xml = robot_xml->FirstChildElement("end_effector"); eef_xml;
+  for (XMLElement* eef_xml = robot_xml->FirstChildElement("end_effector"); eef_xml;
        eef_xml = eef_xml->NextSiblingElement("end_effector"))
   {
     const char* ename = eef_xml->Attribute("name");
@@ -434,9 +436,9 @@ void srdf::Model::loadEndEffectors(const urdf::ModelInterface& urdf_model, TiXml
   }
 }
 
-void srdf::Model::loadLinkSphereApproximations(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+void srdf::Model::loadLinkSphereApproximations(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* cslink_xml = robot_xml->FirstChildElement("link_sphere_approximation"); cslink_xml;
+  for (XMLElement* cslink_xml = robot_xml->FirstChildElement("link_sphere_approximation"); cslink_xml;
        cslink_xml = cslink_xml->NextSiblingElement("link_sphere_approximation"))
   {
     int non_0_radius_sphere_cnt = 0;
@@ -457,7 +459,7 @@ void srdf::Model::loadLinkSphereApproximations(const urdf::ModelInterface& urdf_
 
     // get the spheres for this link
     int cnt = 0;
-    for (TiXmlElement *sphere_xml = cslink_xml->FirstChildElement("sphere"); sphere_xml;
+    for (XMLElement* sphere_xml = cslink_xml->FirstChildElement("sphere"); sphere_xml;
          sphere_xml = sphere_xml->NextSiblingElement("sphere"), cnt++)
     {
       const char* s_center = sphere_xml->Attribute("center");
@@ -531,9 +533,9 @@ void srdf::Model::loadLinkSphereApproximations(const urdf::ModelInterface& urdf_
   }
 }
 
-void srdf::Model::loadDisabledCollisions(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+void srdf::Model::loadDisabledCollisions(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* c_xml = robot_xml->FirstChildElement("disable_collisions"); c_xml;
+  for (XMLElement* c_xml = robot_xml->FirstChildElement("disable_collisions"); c_xml;
        c_xml = c_xml->NextSiblingElement("disable_collisions"))
   {
     const char* link1 = c_xml->Attribute("link1");
@@ -563,10 +565,14 @@ void srdf::Model::loadDisabledCollisions(const urdf::ModelInterface& urdf_model,
   }
 }
 
-void srdf::Model::loadPassiveJoints(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+void srdf::Model::loadPassiveJoints(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
-  for (TiXmlElement* c_xml = robot_xml->FirstChildElement("passive_joint"); c_xml;
-       c_xml = c_xml->NextSiblingElement("passive_joint"))
+  for (XMLElement* c_xml = robot_xml->FirstChildElement("passive_joint"); c_xml; c_xml = c_xml->NextSiblingElement("pas"
+                                                                                                                   "siv"
+                                                                                                                   "e_"
+                                                                                                                   "joi"
+                                                                                                                   "n"
+                                                                                                                   "t"))
   {
     const char* name = c_xml->Attribute("name");
     if (!name)
@@ -592,10 +598,10 @@ void srdf::Model::loadPassiveJoints(const urdf::ModelInterface& urdf_model, TiXm
   }
 }
 
-bool srdf::Model::initXml(const urdf::ModelInterface& urdf_model, TiXmlElement* robot_xml)
+bool srdf::Model::initXml(const urdf::ModelInterface& urdf_model, XMLElement* robot_xml)
 {
   clear();
-  if (!robot_xml || robot_xml->ValueStr() != "robot")
+  if (!robot_xml || strcmp(robot_xml->Value(), "robot") != 0)
   {
     CONSOLE_BRIDGE_logError("Could not find the 'robot' element in the xml file");
     return false;
@@ -624,9 +630,9 @@ bool srdf::Model::initXml(const urdf::ModelInterface& urdf_model, TiXmlElement* 
   return true;
 }
 
-bool srdf::Model::initXml(const urdf::ModelInterface& urdf_model, TiXmlDocument* xml)
+bool srdf::Model::initXml(const urdf::ModelInterface& urdf_model, XMLDocument* xml)
 {
-  TiXmlElement* robot_xml = xml ? xml->FirstChildElement("robot") : NULL;
+  XMLElement* robot_xml = xml ? xml->FirstChildElement("robot") : NULL;
   if (!robot_xml)
   {
     CONSOLE_BRIDGE_logError("Could not find the 'robot' element in the xml file");
@@ -660,11 +666,11 @@ bool srdf::Model::initFile(const urdf::ModelInterface& urdf_model, const std::st
 
 bool srdf::Model::initString(const urdf::ModelInterface& urdf_model, const std::string& xmlstring)
 {
-  TiXmlDocument xml_doc;
+  XMLDocument xml_doc;
   xml_doc.Parse(xmlstring.c_str());
   if (xml_doc.Error())
   {
-    CONSOLE_BRIDGE_logError("Could not parse the SRDF XML File. %s", xml_doc.ErrorDesc());
+    CONSOLE_BRIDGE_logError("Could not parse the SRDF XML File. %s", xml_doc.ErrorStr());
     return false;
   }
   return initXml(urdf_model, &xml_doc);
